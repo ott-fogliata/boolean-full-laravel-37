3c46d6412eda8bfd8405e5d665a6cddba24b24be
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2021-09-13 13:07:53 +0200

    auth

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,56 +1,47 @@
 <?php
 
-namespace App\Http\Controllers;  
+namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;  // invece del require_once, ma fa riferimento sempre a classi (o a traits)
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Book;
+
 
 class HomeController extends Controller
 {
-    
-    public function index() {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');  
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+
         /*
-        $dato = 'un valore';
-        $dato1 = 'un altro valore';
-        $dato2 = 'ancora un valore';
+        $user = Auth::user();
+        if(empty($user)) {
+            return 'You must be logged';
+        }
+        */
 
-        return view('home', [
-            'dato' => $dato,
-            'dato1' => $dato1
-            ...
-        ]); */
-        // [ 'dato' => 'un valore' ]
+        $user = Auth::user();
+        dump($user);
 
-        // dump(compact('dato', 'dato1', 'dato2'));
-        // die();
-        // return view('home', compact('dato', 'dato1', 'dato2'));
-
-        // SELECT * FROM books
         $allBooks = Book::all();
 
         // Bonus:
         $bookFiltrati = Book::where('price', '>', 10)->get();
 
         return view('home', compact('allBooks', 'bookFiltrati'));
-
-
-        //dump($allBooks);
-        //dump($allBooks[0]->title);
-
-        /* il compact darà:
-
-            [
-                'allBooks' => [
-                    // libro 1,
-                    // libro 2,
-                    // libro 3
-                ]
-            ];
-        */
-
-
     }
-
 }
-
-
